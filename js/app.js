3e34692e448fdaa0a60f323f5df5ad684368d79b
rudevdr <gudayschief@gmail.com>
2018-08-04 17:09:34 +0530

    completed everything

--- js/app.js
+++ js/app.js
@@ -3,21 +3,47 @@
  */
 var deck = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb", "fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
 
-// Randomizes cards on board and updates card HTML
-function updateCards() {
-    deck = shuffle(deck);
-    var index = 0;
-    $.each($(".card i"), function(){
-      $(this).attr("class", "fa " + deck[index]);
-      index++;
-    });
+function init(){
+	cardsArrange();
+
+let timerRunning = false;
+timeInSeconds = 0;
+var dealingCards = []
+numOfMoves = 0;
+}
+
+
+function cardsArrange() {
+
+	deck = shuffle(deck);
+	var index = 0;
+	$.each($(".card i"), function(){
+		$(this).attr("class", "fa " + deck[index]);
+		index++;
+	});
 };
 
+document.querySelector(".win-dialog").style.visibility = "hidden";
+document.querySelector(".deck").style.opacity = "1";
+
+enableClicking = true;
 showingCard = false;
-var dealingCards = []
 matchedCard = 0;
 stars = 3;
-numOfMoves = 0;
+
+function timerManage() {
+	if (timerRunning) {
+		//To Update Moves Counter
+		timeInSeconds++;
+		$(".timer").text(timeInSeconds);
+		setTimeout(timerManage, 1000);
+	}
+	else {
+		timeInSeconds = timeInSeconds;
+		$(".timer").text(timeInSeconds);
+
+	}
+}
 
 /*
  * Display the cards on the page
@@ -28,17 +54,17 @@ numOfMoves = 0;
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+	var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
 
-    return array;
+	return array;
 }
 
 
@@ -61,6 +87,57 @@ function displayCard(card) {
 	}
 }
 
+function displayAndMatch(card) {
+	displayCard(card)
+	showingCard = false;
+	moveAndRating();
+	//
+	//also check here if game is finshed
+	if (dealingCards[0].children().attr("class") === dealingCards[1].children().attr("class")) {
+		matchedCard++;
+		dealingCards = [];
+	}
+	else {
+		setTimeout(notMatchedCard, 600);
+	}
+
+	checkWon();
+}
+
+var onRestart = function() {
+	//Resetting the timer
+	timerRunning = false;
+	timeInSeconds = 0;
+	$(".timer").text(timeInSeconds);
+
+	//Resetting the number of moves
+	numOfMoves = 0;
+	$(".moves").text(numOfMoves);
+
+	//Resetting the number of Rating
+	$(".fa-star-o").attr("class", "fa fa-star");
+
+	//Hide all cards
+	showingCard = false;
+	$.each($(".card"), function(){
+		$(this).removeClass("open");
+		$(this).removeClass("show");
+	});
+	dealingCards = []
+
+	//Making matched cards 0
+	matchedCard = 0
+
+	enableClicking = true;
+	document.querySelector(".win-dialog").style.visibility = "hidden";
+	document.querySelector(".deck").style.opacity = "1";
+
+	document.querySelector(".timer").style.display = "inline"
+	document.querySelector(".timer-unit").style.display = "inline"
+
+	//Shuffling the cards on Reset
+	cardsArrange();
+}
 
 function notMatchedCard(card) {
 	dealingCards.forEach(function(card) {
@@ -68,56 +145,79 @@ function notMatchedCard(card) {
 		card.toggleClass("show");
 	});
 
-    dealingCards = [];
+	dealingCards = [];
 }
 
-function matchedCard(card) {
+function moveAndRating() {
 
-    dealingCards.forEach(function(card) {
-        card.addClass("match");
-    });
+	//To Update Moves Counter
+	numOfMoves++;
+	$(".moves").text(numOfMoves);
 
-
-}
-
-function displayAndMatch(card) {
-	displayCard(card)
-	showingCard = false;
-	//also check here if game is finshed
-	if (dealingCards[0].children().attr("class")===dealingCards[1].children().attr("class")) {
-		setTimeout(matchedCard, 100);
-		dealingCards = [];
-	}
-	else {
-		setTimeout(notMatchedCard, 300);
+	//To Update Rating Counter
+	if (numOfMoves === 14 || numOfMoves === 20 ) {
+		let stars = $(".fa-star");
+		$(stars[stars.length-1]).toggleClass("fa-star fa-star-o");
 	}
 }
 
-function checkWon(){
-
-}
-
-function onRestart() {
 
 
-}
+function checkWon() {
+	if (matchedCard === 1){
+		timerRunning = false;
 
-$( ".card" ).click(function() {
+		document.querySelector(".win-dialog").style.visibility = "inherit";
+		enableClicking = false;
+		document.querySelector(".deck").style.opacity = "0.2";
 
-	//Display only if card is not being displayed and is not matched
-	if (!$(this).hasClass("open") || $(this).hasClass("match")) {
+		$("#win-time").text(timeInSeconds);
 
-		//If a card is already being shown then display both and check if they match and if they match then check if game done
-		if(showingCard) {
-			displayAndMatch( $(this) )
-
+		if (numOfMoves <= 14) {
+			star = "3"
+		}
+		else if (numOfMoves <= 20 ) {
+			star = "2"
 		}
 		else {
-			showingCard = true;
-			displayCard( $(this) )
-
+			star = "1"
 		}
-	}
-});
 
-$(updateCards);
+		$("#win-stars").text(star);
+
+		$("#win-repeat").click(onRestart);
+
+		document.querySelector(".timer").style.display = "none"
+		document.querySelector(".timer-unit").style.display = "none"
+	}
+};
+
+
+
+
+var onClick = function() {
+	if (enableClicking){
+		//Display only if card is not being displayed and is not matched
+		if (!$(this).hasClass("open") || $(this).hasClass("match")) {
+
+			//If a card is already being shown then display both and check if they match and if they match then check if game done
+			if(showingCard && dealingCards.length === 1 ) {
+				displayAndMatch( $(this) )
+			}
+			else if (!showingCard && dealingCards.length < 1) {
+				showingCard = true;
+				displayCard( $(this) )
+			}
+		}
+		if (!timerRunning) {
+			timerRunning = true;
+			setTimeout(timerManage, 1000);
+		}
+
+	}
+};
+
+$(".card").click(onClick);
+$(".restart").click(onRestart);
+
+$(init);
